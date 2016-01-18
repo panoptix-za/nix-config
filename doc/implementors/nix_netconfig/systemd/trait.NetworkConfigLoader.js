02c5b6997ee580ca3a627c0c8e620f7cbe600889
Stephan Buys <stephan.buys@panoptix.co.za>
2016-01-18 20:49:14 +0200

    Update docs for ca4d1adec31fbb2a1630ba9cec606b13ec10eaa3
    Remove commented code, fix indentation and remove whitespace at top of files

---
+++ doc/implementors/nix_netconfig/systemd/trait.NetworkConfigLoader.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['nix_netconfig'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
